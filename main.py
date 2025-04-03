6d66742aa7e43abff8fbde7814437fcb30900a4c
SoCaBa <solucionescabalrry@gmail.com>
2025-04-03 18:09:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Servidor en Render funcionando correctamente"
+@app.route('/update_status', methods=['POST'])
+def update_status():
+    data = request.json
+    print(f"Recibido: {data}")
+    return jsonify({"message": "Estado actualizado"}), 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/get_commands', methods=['GET'])
+def get_commands():
+    return jsonify({"relay1": "ON", "relay2": "OFF"}), 200
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
